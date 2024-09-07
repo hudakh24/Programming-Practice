1920bb89ea499d2579a04a1d3fe2363e1d50102a
hudakh24 <hudashoaib2000@gmail.com>
2024-09-07 15:33:21 +0500

    Merge two sorted arrays into a new array.

--- merge_two_sorted_arrays.cpp
+++ merge_two_sorted_arrays.cpp
@@ -14,10 +14,10 @@ void displayArray(int arr[], int size) {
     cout << endl;
 }
 
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size-1; i++) {
         int minIndex = i;
-        for (int j = i+1; j < n; j++) {
+        for (int j = i+1; j < size; j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
