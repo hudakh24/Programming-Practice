4a5dca08f03a48b1967d724ef73632b98efd0b88
hudakh24 <hudashoaib2000@gmail.com>
2024-09-08 23:50:36 +0500

    Find the frequency of characters in a string with a time complexity of O(n).

--- freq_of_char_in_string.cpp
+++ freq_of_char_in_string.cpp
@@ -2,24 +2,29 @@
 #include <string>
 using namespace std;
 
-void findCharFreq(string str){
-    int size = str.length();
-    for (int i; i < size; i++)
-    {
-        int count = 1;
-        char temp;
-        temp = str[i];
-        if(temp==str[i+1]){
-            count = count + 1;
+void findCharFreq(string str) {
+    int freq[256] = {0};  // Array to store the frequency of each character (ASCII range)
+
+    // First loop: Count frequency of each character
+    for (int i = 0; i < str.length(); i++) {
+        char temp = str[i];  // Get current character
+        freq[(int)temp]++;  // Increase the frequency of the character
+    }
+
+    // Second loop: Print frequency of each character
+    for (int i = 0; i < str.length(); i++) {
+        char temp = str[i];
+        if (freq[(int)temp] != 0) {  // Only print if the character hasn't been printed yet
+            cout << "Frequency of '" << temp << "' is: " << freq[(int)temp] << endl;
+            freq[(int)temp] = 0;  // Set frequency to 0 to avoid printing again
         }
-        cout << "frequency of " << str[i] << " is: " << count << endl;
     }
 }
 
 int main() {
     string str;
     cout << "Enter a string: ";
-    getline(cin, str); // This will capture the entire line, including spaces
-    cout << "You entered: " << str << endl;
+    getline(cin, str);  // Input the entire string, including spaces
+
     findCharFreq(str);
 }
